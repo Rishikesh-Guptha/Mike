27abb2bd31c7e63a0b439c06f38675e2ff378c3e
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2023-10-08 18:11:52 +0530

    Addded all the assets required for the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 /** @format */
 
 export default function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	return (
+		<h1 className="text-3xl font-medium bg-blue-300 text-center pt-2 pb-2">
+			Hello world!
+		</h1>
+	);
 }
